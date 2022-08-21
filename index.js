2e0bcfbfe9ce10f0cc12a55a8bf12826c7578926
AlexandraBautistaChattin <balexandra96@gmail.com>
2022-08-21 10:10:01 -0700

    adding cards

--- index.js
+++ index.js
@@ -1,31 +1,14 @@
-let losses = 0;
-let wins = 0;
+var losses = 0;
+var cardList = [
+    "bulbasaur",
+    "charmander",
+    "cyndaquil",
+    "eevee",
+    "jigglypuff",
+    "mew",
+    "pikachu",
+    "ponyta",
+    "squirtle",
+    "togepi",
 
-let cardList = ["darkness", "double", "fairy", "fighting", "fire", "grass", "lightening", "metal", "psychic", "water"]
-
-let cardSet;
-let board = [];
-let rows = 4;
-let colum = 5;
-
-window.onload = function() {
-    shuffleCards();
-    startGame();
-}
-
-//actually getting the board set up
-
-function shuffleCards() {
-    cardSet = cardList.concat(cardList);
-    console.log(cardSet);
-}
-    for (let i = 0; i < cardSet.length; i++) {
-        let j = Math.floor(Math.random() * cardSet.length);
-
-        let temp = cardSet[i];
-        cardSet[i] = cardSet[j];
-        cardSet [j] = temp;
-    }
-        console.log(cardSet);
-
-
+]
